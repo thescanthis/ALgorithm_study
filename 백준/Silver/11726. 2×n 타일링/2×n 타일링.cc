9ecad890db60e34e957017c6efdaa6e3093fad65
thescant <70213672+thescanthis@users.noreply.github.com>
2024-06-14 21:35:15 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    int arr[1001] = {};
+
+    arr[1] = 1;
+    arr[2] = 2;
+    for (int i = 3; i <= n; i++)
+        arr[i] = (arr[i - 1] + arr[i - 2])%10007;
+    std::cout << arr[n];
+
+    return 0;
+}
