5465e9c9863abc93e270319e3f391653b7e20746
thescant <70213672+thescanthis@users.noreply.github.com>
2024-06-14 21:26:06 +0900

    [Silver III] Title: N과 M (7), Time: 672 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define SIZE 10
+
+int n,m;
+std::vector<int> arr,v;
+
+void Input()
+{
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        std::cin>>x;
+        arr.push_back(x);
+    }
+    sort(arr.begin(),arr.end());
+}
+
+void Print()
+{
+    for(int i=0; i<m; i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    cout<<'\n';
+}
+
+void DFS(int cnt)
+{
+    if(cnt==m)
+    {
+        Print();
+        return;
+    }
+    
+    for(int i=0; i<n; i++)
+    {
+        v.push_back(arr[i]);
+        DFS(cnt+1);
+        v.pop_back();
+    }
+}
+
+
+int main() {
+    
+    std::cin>>n>>m;
+    Input();
+    DFS(0);
+    return 0;
+}
