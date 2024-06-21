7bbbf8f94acbd417b1dfa9b3d626311093d27537
thescant <70213672+thescanthis@users.noreply.github.com>
2024-06-21 16:36:59 +0900

    [Bronze II] Title: 단어의 개수, Time: 44 ms, Memory: 4916 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int cnt=0;
+    while(true)
+    {
+        string str;
+        std::cin>>str;
+        if(str=="") break;
+        cnt++;
+    }
+    cout<<cnt;
+    return 0;
+}
