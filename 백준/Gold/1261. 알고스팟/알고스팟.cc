8ba2e9946e6722223a41d4ddba48a709e5b34f48
thescant <70213672+thescanthis@users.noreply.github.com>
2024-07-18 19:39:07 +0900

    [Gold IV] Title: 알고스팟, Time: 4 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.cc
+++ 백준/Gold/1261. 알고스팟/알고스팟.cc
@@ -1,74 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
-vector<vector<int>> ans;
-vector<vector<int>> Map;
+int n,m;
+int my[4]= {0,1,0,-1};
+int mx[4]= {1,0,-1,0};
 
+std::vector<vector<int>> Map;
+std::vector<vector<int>> Dijistra;
 
 void Input()
 {
-    ans = vector<vector<int>>(m, vector<int>(n, 0));
-    Map = vector<vector<int>>(m, vector<int>(n, 0));
-    for (int i = 0; i < m; i++)
+    Map = vector<vector<int>>(m,vector<int>(n,0));
+    Dijistra = vector<vector<int>>(m,vector<int>(n,0));
+    for(int i=0; i<m; i++)
     {
-        string str;
-        std::cin >> str;
-        for (int j = 0; j < str.size(); j++)
+        std::string str;
+        std::cin>>str;
+        for(int j=0; j<str.size(); j++)
         {
-            Map[i][j] = str[j] - '0';
-            ans[i][j] = INT_MAX;
+            Map[i][j]=str[j]-'0';
+            Dijistra[i][j]=1001;
         }
     }
 }
 
-void BFS(int y, int x)
+void BFS(int y,int x)
 {
-    queue<pair<int, int>> q;
-    q.push({ y,x });
-    ans[y][x] = 0;
-
-    int my[4] = {0,1,0,-1};
-    int mx[4] = {1,0,-1,0};
-
-    while (!q.empty())
+    queue<pair<int,int>> q;
+    q.push({y,x});
+    Dijistra[y][x]=0;
+    while(!q.empty())
     {
-        int y_front = q.front().first;
-        int x_front = q.front().second;
+        int y_f = q.front().first;
+        int x_f = q.front().second;
         q.pop();
-        for (int i = 0; i < 4; i++)
+        
+        for(int i=0; i<4; i++)
         {
-            int _my = my[i] + y_front;
-            int _mx = mx[i] + x_front;
-
-            if ((_my < 0 || _mx < 0) || (_my >= m || _mx >= n)) continue;
-
-            if (Map[_my][_mx] == 1)
+            int _my = my[i]+y_f;
+            int _mx = mx[i]+x_f;
+            
+            if(_my<0 || _mx<0 || _my >= m || _mx>=n) continue;
+            
+            if(Map[_my][_mx]==0)
             {
-                if (ans[_my][_mx] > ans[y_front][x_front] + 1)
+                if(Dijistra[_my][_mx]>Dijistra[y_f][x_f])
                 {
-                    ans[_my][_mx] = ans[y_front][x_front] + 1;
-                    q.push({ _my,_mx });
+                    Dijistra[_my][_mx] = Dijistra[y_f][x_f];
+                    q.push({_my,_mx});
                 }
             }
-            else if (Map[_my][_mx] == 0)
+            else if(Map[_my][_mx]==1)
             {
-                if (ans[_my][_mx] > ans[y_front][x_front])
+                if(Dijistra[_my][_mx]>Dijistra[y_f][x_f]+1)
                 {
-                    ans[_my][_mx] = ans[y_front][x_front];
-                    q.push({ _my,_mx });
+                    Dijistra[_my][_mx] = Dijistra[y_f][x_f]+1;
+                    q.push({_my,_mx});
                 }
             }
+            
         }
     }
 }
 
-
-int main()
-{
-    std::cin >> n >> m;
+int main() {
+    std::cin>>n>>m;
     Input();
-    BFS(0, 0);
-    cout << ans[m - 1][n - 1];
+    BFS(0,0);
+    cout<<Dijistra[m-1][n-1];
     return 0;
 }
