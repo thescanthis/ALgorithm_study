7c3103d3e3beb2b6cc8a9b52978cc0b124aff31a
thescant <70213672+thescanthis@users.noreply.github.com>
2024-10-02 21:49:45 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,47 +1,66 @@
-#include <iostream>
-#include <cstring>
-#include <queue>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
 
-std::vector<int>edge[1001];
-bool check[1001];
+vector<vector<int>> edge(1001);
+bool vist[1001] = {};
 
-void DFS(int v)
+void DFS(int idx)
 {
-    if(check[v]) return;
-    check[v]=true;
-    std::cout<<v<<' '; //방문하고 출력
-    for(int to : edge[v]) DFS(to); // 연결된 모든 정점 탐색진행;
-    //루프를 빠져나왔다는건 v에 연결되어져 있는 모든 정점을 완료했다는것
+	if (vist[idx]) return;
+	vist[idx] = true;
+
+	cout << idx<<" ";
+
+	for (auto x : edge[idx]) DFS(x);
 }
 
-void BFS(int v){
-    std::queue<int>q;
-    q.push(v); check[v]=true;
-    
-    while(!q.empty()){
-        int from=q.front(); q.pop();
-        std::cout<<from<<' ';
-        for(int to : edge[from]){
-            if(check[to]==false){q.push(to); check[to]=true;}
-        }
-    }
+void _BFS(int idx)
+{
+	queue<int> q;
+	q.push(idx);
+
+	vist[idx] = true;
+
+	while (!q.empty())
+	{
+		int front = q.front();
+		q.pop();
+
+		cout << front << " ";
+		for (int key : edge[front])
+		{
+			if (!vist[key])
+			{
+				vist[key] = true;
+				q.push(key);
+			}
+		}
+	}
 }
 
-int main(void)
+int main()
 {
-    int n,m,start;
-    std::cin>>n>>m>>start;
-    while(m--){
-        int f,t; std::cin>>f>>t;
-        edge[f].emplace_back(t);
-        edge[t].emplace_back(f);
-    }
-    for(int i=1; i<1001; i++) sort(edge[i].begin(),edge[i].end());
-    
-    DFS(start);
-    std::cout<<'\n';
-    memset(check,false,sizeof(check));
-    BFS(start);
+	int n, m, v;
+	std::cin >> n >> m >> v;
+
+	while (m--)
+	{
+		int x, y;
+		std::cin >> x >> y;
+
+		edge[x].push_back(y);
+		edge[y].push_back(x);
+	}
+
+	for (int i = 0; i < edge.size(); i++)
+		sort(edge[i].begin(), edge[i].end());
+
+	memset(vist, false, sizeof(vist));
+	DFS(v);
+
+	memset(vist, false, sizeof(vist));
+	cout << '\n';
+	_BFS(v);
+
+	return 0;
 }
