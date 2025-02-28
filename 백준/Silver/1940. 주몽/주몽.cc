5b8b3ab20b87b881d0e4f61c61b2f3f45cf83d98
thescant <70213672+thescanthis@users.noreply.github.com>
2025-02-28 21:25:01 +0900

    [Silver IV] Title: 주몽, Time: 88 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -1,28 +1,44 @@
-// Online C++ compiler to run C++ program online
-#include <bits/stdc++.h>
+// BigStone.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string.h>
 using namespace std;
 
-int n,m;
+int n, m;
 vector<int> v;
-int sum,cnt;
 
-int main() {
-    cin>>n>>m;
-    
-    for(int i=0; i<n; i++)
-    {
-        int x;
-        std::cin>>x;
-        v.push_back(x);
-    }
-    
-    for(int i=0; i<v.size(); i++)
-    {
-        for(int j=i+1; j<v.size(); j++)
-        {
-            if(v[i]+v[j]==m)cnt++;
-        }
-    }
-    cout<<cnt;
-    return 0;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+	int ans = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (v[i] + v[j] == m)ans++;
+		}
+	}
+	cout << ans;
+	return 0;
 }
