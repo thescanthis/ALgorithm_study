c08c036af29f2fea0aa1fe8aa6a2f0c2a6a2aae5
thescant <70213672+thescanthis@users.noreply.github.com>
2025-05-02 17:22:33 +0900

    [Platinum V] Title: 백조의 호수, Time: 124 ms, Memory: 16740 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.cc
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.cc
@@ -12,108 +12,122 @@
 #include <algorithm>
 #include <string.h>
 using namespace std;
-#define SZ 1504
+typedef long long ll;
+#define _SIZE 1501
 
 int n, m;
+char arr[_SIZE][_SIZE] = {};
+bool visited[_SIZE][_SIZE] = {};
+bool water[_SIZE][_SIZE] = {};
 int dy[4] = { -1,0,1,0 };
 int dx[4] = { 0,1,0,-1 };
-
-int visted[SZ][SZ],water[SZ][SZ];
-char arr[SZ][SZ];
-int _Start, _End;
-int y, x;
+int f, t;
 queue<pair<int, int>> waterQ;
 
-bool FindSwan(queue<pair<int, int>>& q)
+int FindSwan(queue<pair<int,int>>& q)
 {
-    visted[q.front().first][q.front().second] = true;
-    queue<pair<int, int>> Temp;
-    while (q.size())
-    {
-        tie(y, x) = q.front();
-        q.pop();
-        
-        for (int i = 0; i < 4; i++)
-        {
-            int ny = dy[i] + y;
-            int nx = dx[i] + x;
+	visited[q.front().first][q.front().second] = true;
+	queue<pair<int, int>> tempQ;
 
-            if (ny < 0 || nx < 0 || ny >= n || nx >= m || visted[ny][nx]) continue;
-            visted[ny][nx] = true;
+	while (q.size())
+	{
+		tie(f, t) = q.front();
+		q.pop();
 
-            if (arr[ny][nx] == 'L')
-                return true;
-            else if (arr[ny][nx] == '.')
-                q.push({ ny,nx });
-            else if (arr[ny][nx] == 'X')
-                Temp.push({ ny,nx });
-        }
-    }
+		for (int i = 0; i < 4; i++)
+		{
+			int _dy = dy[i] + f;
+			int _dx = dx[i] + t;
 
-    q = Temp;
-    return false;
+			if (_dy < 0 || _dx < 0 || _dy >= n || _dx >= m || visited[_dy][_dx]) continue;
+		
+			visited[_dy][_dx] = true;
+
+			if (arr[_dy][_dx] == 'L')
+				return true;
+			else if (arr[_dy][_dx] == '.')
+			{
+				q.push({ _dy,_dx });
+			}
+			else if (arr[_dy][_dx] == 'X')
+			{
+				tempQ.push({ _dy,_dx });
+			}
+			
+		}
+	}
+	q = tempQ;
+	return false;
 }
 
 void FindWater()
 {
-    queue<pair<int, int>> Temp;
-    while (waterQ.size())
-    {
-        tie(y, x) = waterQ.front();
-        waterQ.pop();
+	queue<pair<int, int>> tempQ;
 
-        for (int i = 0; i < 4; i++)
-        {
-            int ny = dy[i] + y;
-            int nx = dx[i] + x;
+	while (waterQ.size())
+	{
+		tie(f, t) = waterQ.front();
+		waterQ.pop();
 
-            if (ny < 0 || nx < 0 || ny >= n || nx >= m || water[ny][nx]) continue;
-            water[ny][nx] = true;
-            if (arr[ny][nx] == 'X')
-            {
-                Temp.push({ ny,nx });
-                arr[ny][nx] = '.';
-            }
-        }
-    }
+		for (int i = 0; i < 4; i++)
+		{
+			int _dy = dy[i] + f;
+			int _dx = dx[i] + t;
 
-    waterQ = Temp;
+			if (_dy < 0 || _dx < 0 || _dy >= n || _dx >= m || water[_dy][_dx]) continue;
+
+			water[_dy][_dx] = true;
+			if (arr[_dy][_dx] == 'X')
+			{
+				tempQ.push({ _dy,_dx });
+				arr[_dy][_dx] = '.';
+			}
+		}
+	}
+
+	waterQ = tempQ;
+
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    cin >> n >> m;
+	ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+	std::cout.tie(0);
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cin >> arr[i][j];
-            if (arr[i][j] == 'L') {
-                _Start = i;
-                _End = j;
-            }
 
-            if (arr[i][j] == 'L' || arr[i][j] == '.')
-            {
-                waterQ.push({ i,j });
-            }
-        }
-    }
+	cin >> n >> m;
 
-    int ans = 0;
-    queue<pair<int, int>> q;
-    q.push({ _Start,_End });
-    while (waterQ.size())
-    {
-        if (FindSwan(q)) break;
-        FindWater();
-        ans++;
-    }
 
-    cout << ans;
-    return 0;
+	int y, x;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> arr[i][j];
+			if (arr[i][j] == 'L')
+			{
+				y = i;
+				x = j;
+				waterQ.push({ i,j });
+			}
+
+			if (arr[i][j] == '.')
+				waterQ.push({ i,j });
+		}
+
+	}
+
+	queue<pair<int, int>> q;
+	q.push({ y,x });
+	int cnt = 0;
+	while (waterQ.size())
+	{
+		if (FindSwan(q)) break;
+		FindWater();
+		cnt++;
+	}
+
+	cout << cnt;
+	return 0;
 }
