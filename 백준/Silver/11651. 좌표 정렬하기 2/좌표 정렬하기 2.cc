9536998eb69cdc4714ac55f1ecff86291d65646c
thescant <70213672+thescanthis@users.noreply.github.com>
2025-07-14 21:07:48 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 52 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,50 @@
+// BigStone.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string.h>
+using namespace std;
+
+vector<pair<int,int>> v;
+int n;
+
+bool comapre(pair<int,int>&p1,pair<int,int>& p2)
+{
+	if (p1.second != p2.second)
+		return p1.second < p2.second;
+	return p1.first < p2.first;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+	std::cout.tie(0);
+	
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		int y, x;
+		cin >> y >> x;
+		v.push_back({ y,x });
+	}
+
+	sort(v.begin(), v.end(), comapre);
+	
+
+	for (auto x : v)
+	{
+		cout << x.first << " " << x.second << '\n';
+	}
+	
+	return 0;
+}
