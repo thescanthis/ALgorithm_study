561a165b8b956faec4875fbdfec974bbe9bd0059
thescant <70213672+thescanthis@users.noreply.github.com>
2025-07-14 21:14:48 +0900

    [Silver V] Title: 단어 정렬, Time: 36 ms, Memory: 7464 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,30 +1,54 @@
-#include<iostream>
-#include<set>
-#include<vector>
+// BigStone.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string.h>
 using namespace std;
 
-int main(void)
+int n;
+vector<string> v;
+
+bool compare(string& str, string& str1)
 {
-    int n;
-    cin>>n;
-    
-    set<pair<int,string>> s;
-    int temp;
-    for(int i=0; i<n; i++)
-    {
-        string str;
-        cin>>str;
-        temp=0;
-        for(int i=0; i<str.size(); i++)
-        {
-            temp++;
-        }
-        s.emplace(temp,str);
-    }
-    
-    for(auto x:s)
-    {
-        cout<<x.second<<'\n';
-    }
-    return 0;
+	if (str.size() != str1.size())
+		return str.size() < str1.size();
+
+	return str < str1;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+	std::cout.tie(0);
+	
+	cin >> n;
+	set<string> s;
+
+	for (int i = 0; i < n; i++)
+	{
+		string str;
+		cin >> str;
+		s.emplace(str);
+	}
+
+	for (auto x : s)
+		v.push_back(x);
+	
+	sort(v.begin(), v.end(),compare);
+
+	for (auto x : v)
+	{
+		cout << x << '\n';
+	}
+
+	return 0;
 }
